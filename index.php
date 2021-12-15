0848f66effc24e6a5156c05096ed85c8cd6689df
Ronobi <amir15-13026@diu.edu.bd>
2021-12-16 01:03:59 +0600

    club section

--- index.php
+++ index.php
@@ -160,20 +160,151 @@
           </div>
         </div>
       </section>
+          <!-- club start -->
+      <section class=" clubs pt-5 pb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <h3 class="mb-3">Diu clubs </h3>
+                </div>
+                <div class="col-6 text-end">
+                    <a class="btn btn-primary mb-3 mr-1" href="#carouselExampleIndicators2" role="button" data-bs-slide="prev">
+                        <i class="fa fa-arrow-left"></i>
+                    </a>
+                    <a class="btn btn-primary mb-3 mr-1" href="#carouselExampleIndicators2" role="button" data-bs-slide="next">
+                        <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+                <div class="col-12">
+                    <div id="carouselExampleIndicators2" class="carousel slide" data-bs-ride="carousel">
+    
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <div class="row">
+    
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/1.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title text-center">Computer & Programming Club</h4>
+                                                <p class="card-text">DIU CPC is the most primitive and extensive club as well as the biggest club in Daffodil International University. We work together to explore every field of Computer Science.</p>
+                                                <a href="http://cpc.daffodilvarsity.edu.bd/#" class="btn btn-primary mt-4">Explore more</a>
+    
+                                            </div>
+    
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/5.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title text-center">GIRLS' COMPUTER PROGRAMMING CLUB</h4>
+                                                <p class="card-text">DIU GCPC is the first girls' computer programming club in Bangladesh. We started our journey from January 2018. </p>
+                                                <a href="http://cpc.daffodilvarsity.edu.bd/#" class="btn btn-primary mt-5">Explore more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/8.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Diu Rotaract Club</h4>
+                                                <p class="card-text">This club one of the active and vibrant club in RID 3281 Bangladesh. Our focus area expanding professional networks, exchange ideas with leaders, and cultivate lifelong friendships around the world, develop skills to become community and global leaders.</p>
+                                                <a href="https://www.facebook.com/racdiu/" class="btn btn-primary mt-1">Explore more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+    
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="row">
+    
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/3.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">DIU Debating club</h4>
+                                                <p class="card-text">Join to enhance your confidence to talk publicly.</p>
+                                                <a href="https://diudc.daffodilvarsity.edu.bd/" class="btn btn-primary">Explore more</a>
+                                            </div>
+    
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/2.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">DIU cyber security club</h4>
+                                                <p class="card-text">Prevent your system from unauthorized access.</p>
+                                                <a href="https://www.facebook.com/CSCDIU" class="btn btn-primary">Explore more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/9.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Diu Textile club</h4>
+                                                <p class="card-text">Join  to develope practical skills.</p>
+                                                <a href="http://diutc.daffodilvarsity.edu.bd/" class="btn btn-primary mt-4">Explore more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+    
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="row">
+    
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/6.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Diu NLP & ML Lab</h4>
+                                                <p class="card-text">Are you interested for research?</p>
+                                                <a href="https://www.facebook.com/diunlp" class="btn btn-primary">Explore more</a>
+                                            </div>
+    
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card h">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/4.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">English literary club</h4>
+                                                <p class="card-text">Are you develop your communication skill?</p>
+                                                <a href="https://www.facebook.com/diunlp" class="btn btn-primary">Explore more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <img class="img-fluid" alt="100%x280" src="./img/club/1/8.jpg">
+                                            <div class="card-body">
+                                                <h4 class="card-title">DIU Robotics</h4>
+                                                <p class="card-text">Want to learn Robotics?</p>
+                                                <a href="https://www.facebook.com/drcdsc2018" class="btn btn-primary">Explore more</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- club end -->
+
 
       <!-- teacher end -->
 
 
 
 
-        <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="./tilt.jquery.min.js"></script>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/popper.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/main.js"></script>
 
   
   </body>
