586cba28334d34b56cad46a820a7821da1c4e1df
Abdur-Rahman-Apu <abdur15-13025@diu.edu.bd>
2021-12-17 08:58:36 +0600

    footer add

--- index.php
+++ index.php
@@ -255,6 +255,8 @@
 
  <!-- question end -->
 
+<?php include("./footer1/index.html"); ?>
+
         <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="./tilt.jquery.min.js"></script>
